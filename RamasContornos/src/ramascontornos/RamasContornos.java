32317e2c95167e8eadb4e17cc5980701fe39330d
antonio <antonio@biruseto>
2016-10-27 09:02:22 +0200

    creacion inicial del programa

---
+++ RamasContornos/src/ramascontornos/RamasContornos.java
@@ -0,0 +1,21 @@
+
+package ramascontornos;
+
+import javax.swing.JOptionPane;
+
+
+public class RamasContornos {
+
+  
+    public static void main(String[] args) {
+        float base;
+        float altura;
+        float area;
+        
+        base=Float.parseFloat(JOptionPane.showInputDialog("base"));
+        altura=Float.parseFloat(JOptionPane.showInputDialog("altura"));
+        area=base*altura/2;
+        System.out.println("area es:"+(base*altura));
+    }
+    
+}
